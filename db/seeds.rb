9daa4b25d8e1b8e0b2b3c9ca9df168217d7851f4
TakuyaMiura <md7090007@gmail.com>
2024-09-02 10:57:26 +0900

    03 create categories table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+major_category_names = ["本", "コンピュータ", "ディスプレイ"]
+book_categories = ["ビジネス", "文学・評論", "人文・思想", "スポーツ",
+                    "コンピュータ・IT", "資格・検定・就職", "絵本・児童書", "写真集",
+                    "ゲーム攻略本", "雑誌", "アート・デザイン", "ノンフィクション"]
+computer_categories = ["ノートPC", "デスクトップPC", "タブレット"]
+display_categories = ["19~20インチ", "デスクトップPC", "タブレット"]
+
+major_category_names.each do |major_category_name|
+    case major_category_name
+        when "本" then
+            category_names = book_categories
+        when "コンピュータ" then
+            category_names = computer_categories
+        when "ディスプレイ" then
+            category_names = display_categories
+    end
+    category_names.each do |category_name|
+        Category.create(
+            major_category_name: major_category_name,
+            name: category_name,
+            description: category_name
+        )
+    end
+end
